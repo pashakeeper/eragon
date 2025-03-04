56d0931c3b622df5a5b19dd14e37c2bdebabd686
pashakeeper <pashagkjunost@gmail.com>
2025-03-04 16:56:51 +0200

    страница факю

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
     var $btn = $('.up');
 
     $(window).on('scroll', function () {
-        if ($(this).scrollTop() > 500) {
+        if ($(this).scrollTop() > 100) {
             $btn.addClass('active');
         } else {
             $btn.removeClass('active');
@@ -60,7 +60,7 @@ $(document).ready(function () {
         $(this).fadeOut();
     });
     Fancybox.bind('[data-fancybox="gallery"]', {});
-    $('.more').on('click', function(e) {
+    $('.more').on('click', function (e) {
         e.preventDefault();
         let currentSection = $(this).closest('section');
         let nextSection = currentSection.next('section');
@@ -70,13 +70,13 @@ $(document).ready(function () {
             }, 500);
         }
     });
-    if ($(window).width()< 991) {
+    if ($(window).width() < 991) {
         $('.galery_row').slick({
             slidesToShow: 1,
             slidesToScroll: 1,
             infinite: false,
             centerPadding: 0,
-            appendDots:$('.dots'),
+            appendDots: $('.dots'),
             // autoplay: true,
             autoplaySpeed: 2000,
             variableWidth: true,
@@ -87,5 +87,44 @@ $(document).ready(function () {
             nextArrow: $('.next')
         });
     }
+    $('.collapse_box .collapse_header').on('click', function () {
+        let content = $(this).next('.collapse_content');
+        let icon = $(this).find('i');
+
+        if (content.is(':visible')) {
+            content.slideUp();
+            icon.removeClass('fa-minus').addClass('fa-plus');
+        } else {
+            $('.collapse_content').slideUp('fast');
+            $('.collapce_header i').removeClass('fa-minus').addClass('fa-plus'); // Меняем все иконки на +
+
+            content.slideDown('fast');
+            icon.removeClass('fa-plus').addClass('fa-minus');
+        }
+    });
+    $('.tabs_list li a').on('click', function (e) {
+        e.preventDefault();
+
+        let tabId = $(this).text().toLowerCase().replace(/\s+/g, '-'); // Генерируем data-id
+        let selectedTab = $('.tabs_content .row[data-id="' + tabId + '"]');
+
+        if (selectedTab.hasClass('active')) return;
+        $('.tabs_list li a').removeClass('active');
+        $('.tabs_content .row').removeClass('active');
+        selectedTab.addClass('active');
+        $(this).addClass('active');
+    });
+    if ($(window).width() < 991) {
+        $('.tabs_list').slick({
+            slidesToShow: 1, // Количество видимых элементов
+            slidesToScroll: 1,
+            infinite: false,
+            variableWidth: true, // Автоматическая ширина
+            arrows: true,
+            prevArrow: '<button class="scroll-btn scroll-left">←</button>',
+            nextArrow: '<button class="scroll-btn scroll-right">→</button>',
+        });
+    }
+
 });
 
